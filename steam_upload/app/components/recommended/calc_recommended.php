697bc889f2f99f1826a63c062b0a9d9f978ec8bb
Willempje2 <willem_huijben@hotmail.com>
2016-11-21 13:28:16 +0100

    Update calc_recommended.php

--- steam_upload/app/components/recommended/calc_recommended.php
+++ steam_upload/app/components/recommended/calc_recommended.php
@@ -2,14 +2,8 @@
 
 /**
  * PHP item based filtering
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * @package   PHP item based filtering
- */
+ * Aangepast + notities erbij
+*/
 
 class Recommend {
 
